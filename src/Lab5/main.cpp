746db8ce2ee66c565ad102d865ffcf4f7cad30e9
SpiritWinner <spirit.no.win@gmail.com>
2020-10-14 17:56:28 +0300

    ...

--- src/Lab5/main.cpp
+++ src/Lab5/main.cpp
@@ -1,64 +1,6 @@
 #include "iostream"
 using namespace std;
 class Matrix;
-/*class Matrix;
-
-class Vector
-{
-private:
-    int vector[3] = {3, 5, 8};
-public:
-    Vector(int vector[])
-    {
-        this->vector;
-    }
-
-    Vector()
-    {
-    }
-
-    friend int* Multiplication(Vector &vec, Matrix &mat);
-
-};
-
-class Matrix
-{
-private:
-    int matrix[3][3] = {{4, 5, 7}, {12, 7, 9}, {8, 5, 1}};
-public:
-    Matrix(int matrix[][3])
-    {
-        this->matrix;
-    }
-
-    Matrix()
-    {
-    }
-
-    friend int* Multiplication(Vector &vec, Matrix &mat);
-};
-
-int* Multiplication(Vector &vec, Matrix &mat)
-{
-    int *out;
-    for (int i = 0; i < sizeof(mat.matrix)/sizeof(mat.matrix[0]); i++)
-    {
-        out[i] = 0;
-        for (int j = 0; j < sizeof(mat.matrix)/sizeof(mat.matrix[0]); j++)
-           out[i] += mat.matrix[i][j] * vec.vector[j];
-    }
-    return out;
-}
-
-
-int main()
-{
-    Matrix matrix;
-    Vector vector;
-    for (auto number : Multiplication(vector, matrix))
-        cout << number << ' ';
-    return 0;
-}*/
 
 class Vector
 {
