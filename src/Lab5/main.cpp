546d8ad8e34a4f6a18e6d1b4382faa7ddde74b87
SpiritWinner <spirit.no.win@gmail.com>
2020-10-14 17:55:52 +0300

    ...

--- src/Lab5/main.cpp
+++ src/Lab5/main.cpp
@@ -1,6 +1,7 @@
 #include "iostream"
 using namespace std;
 class Matrix;
+/*class Matrix;
 
 class Vector
 {
@@ -12,7 +13,11 @@ public:
         this->vector;
     }
 
-    friend int[] Multiplication(Vector &vec, Matrix &mat);
+    Vector()
+    {
+    }
+
+    friend int* Multiplication(Vector &vec, Matrix &mat);
 
 };
 
@@ -26,17 +31,135 @@ public:
         this->matrix;
     }
 
-    friend int[] Multiplication(Vector &vec, Matrix &mat);
-};
-
-int[] Multiplication(Vector &vec, Matrix &mat)
-{
-    int out[100];
-    for (int i = 0; i < sizeof(mat)/sizeof(int); i++)
+    Matrix()
     {
-        out[i] = 0;
-        for (int j = 0; j < sizeof(mat)/sizeof(int); j++)
-           out[i] += mat.matrix[i][j] * vec.vector[j];
     }
 
+    friend int* Multiplication(Vector &vec, Matrix &mat);
+};
+
+int* Multiplication(Vector &vec, Matrix &mat)
+{
+    int *out;
+    for (int i = 0; i < sizeof(mat.matrix)/sizeof(mat.matrix[0]); i++)
+    {
+        out[i] = 0;
+        for (int j = 0; j < sizeof(mat.matrix)/sizeof(mat.matrix[0]); j++)
+           out[i] += mat.matrix[i][j] * vec.vector[j];
+    }
+    return out;
+}
+
+
+int main()
+{
+    Matrix matrix;
+    Vector vector;
+    for (auto number : Multiplication(vector, matrix))
+        cout << number << ' ';
+    return 0;
+}*/
+
+class Vector
+{
+private:
+    int size;
+    int *vec;
+public:
+    Vector (int size)
+    {
+        this->size = size;
+    }
+
+    void inout()
+    {
+        vec = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            cout << "Enter " << i+1 << "number" << endl;
+            cin >> vec[i];
+        }
+    }
+
+    void print()
+    {
+        for (int number = 0; number < size; number++)
+            cout << vec[number] << " ";
+    }
+
+    friend void Multiply(Vector &vec, Matrix &mat);
+};
+
+class Matrix
+{
+private:
+    int **arr;
+    int rows;
+    int columns;
+public:
+    Matrix();
+
+    Matrix(int rows, int columns)
+    {
+        this->rows = rows;
+        this->columns = columns;
+    }
+
+    void inout()
+    {
+        arr = new int*[rows];
+        for (int i = 0; i < rows; i++)
+        {
+            arr[i]  = new int[columns];
+        }
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                cin >> arr[i][j];
+            }
+        }
+    }
+
+    void print()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+    }
+
+    friend void Multiply(Vector &vec, Matrix &mat);
+};
+
+void  Multiply(Vector &vec, Matrix &mat)
+{
+    int out[vec.size];
+    for (int i = 0; i < vec.size; i++)
+    {
+        int temp = 0;
+        for (int j = 0; j < mat.rows; j++) {
+            temp += mat.arr[i][j] * vec.vec[j];
+        }
+        out[i] = temp;
+    }
+
+
+    for (int number = 0; number < vec.size; number++)
+            cout << out[number] << " ";
+}
+
+int main()
+{
+    Vector vector(3);
+    Matrix matrix(3, 3);
+    vector.inout();
+    matrix.inout();
+    //vector.print();
+    //matrix.print();
+    Multiply(vector, matrix);
+    return 0;
 }
