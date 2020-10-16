b97cc7676f1b9ec6241338900084f2f0336c0e05
SpiritWinner <spirit.no.win@gmail.com>
2020-10-16 13:02:44 +0300

    ...

--- src/Lab6/main.cpp
+++ src/Lab6/main.cpp
@@ -1,4 +1,54 @@
 #include "iostream"
+
 using namespace std;
 
+class MyInt
+{
+public:
+    int value;
+public:
+    MyInt(int val)
+    {
+        this->value = val;
+    }
 
+    void print()
+    {
+        cout << value << endl;
+    }
+
+    bool operator! ()
+    {
+        return (value % 2 == 0);
+    }
+
+    friend int operator -- (MyInt &myInt);
+    friend bool operator == (MyInt &myInt1, MyInt &myInt2);
+};
+
+bool operator == (MyInt &myInt1, MyInt &myInt2)
+{
+    return myInt1.value == myInt2.value;
+}
+
+MyInt operator + (MyInt &myInt, int num)
+{
+    return MyInt(myInt.value + num);
+}
+
+int operator -- (MyInt &myInt)
+{
+    return (myInt.value - 3);
+}
+
+int main()
+{
+    MyInt myInt(22);
+    cout << !myInt << endl;
+    cout << --myInt << endl;
+    MyInt myInt1 = myInt + 7;
+    myInt1.print();
+    cout << (myInt == myInt1) << endl;
+
+    return 0;
+}
