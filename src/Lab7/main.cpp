f1a1c9ed34b68c6d0f26e8593a390385487e19bf
SpiritWinner <spirit.no.win@gmail.com>
2020-10-19 14:07:10 +0300

    ---

---
+++ src/Lab7/main.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+const int N = 3;
+
+template <typename Y>
+void trans(Y A[N][N]){
+    Y s;
+    for(int i = 0; i < N; i++)
+        for(int j = i + 1; j < N; j++)
+        {
+            s = A[i][j];
+            A[i][j] = A[j][i];
+            A[j][i] = s;
+        }
+}
+
+template <typename T>
+void show(T A[N][N])
+{
+    for(int i=0; i < N; i++)
+    {
+        for(int j=0; j < N; j++)
+            cout << A[i][j] << " ";
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int in[N][N] =
+            {
+                    { 1, 2, 3 },
+                    { 6, 7, 8 },
+                    { 11, 12, 13 }
+            };
+
+    double du[N][N] =
+            {
+                    { 3.5, 4.8, 6.9 },
+                    { 1.0, 6.6, 4.1 },
+                    { 6.2, 21.2, 1.1 }
+            };
+
+    char ch[N][N] =
+            {
+                    { 's', 'd', 'd' },
+                    { 'o', 'o', 'i' },
+                    { 's', 'g', 'e' }
+            };
+
+    show(ch);
+    trans(ch);
+    cout << "res:" << endl;
+    show(ch);
+
+    show(du);
+    trans(du);
+    cout << "res:" << endl;
+    show(du);
+
+    show(in);
+    trans(in);
+    cout << "res:" << endl;
+    show(in);
+    return 0;
+}
